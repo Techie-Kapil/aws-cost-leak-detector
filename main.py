a478165e82a4e4369516111ddb07b448d314fab0
Techie-Kapil <ksaini0666@gmail.com>
2026-01-20 11:54:07 +0000

    Restore reporting module and reports output folderl

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent
+sys.path.insert(0, str(ROOT_DIR))
+
 import argparse
 
 from scanner.ebs import find_unattached_volumes
